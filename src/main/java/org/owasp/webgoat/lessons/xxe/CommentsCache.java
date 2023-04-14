a6562948082feefef303ed32ccb220f8ee38e764
Rommel Sunga <lightingthefuse@gmail.com>
2023-04-14 17:17:09 +0800

    Update CommentsCache.java

--- src/main/java/org/owasp/webgoat/lessons/xxe/CommentsCache.java
+++ src/main/java/org/owasp/webgoat/lessons/xxe/CommentsCache.java
@@ -96,15 +96,17 @@ public class CommentsCache {
   protected Comment parseXml(String xml) throws JAXBException, XMLStreamException {
     var jc = JAXBContext.newInstance(Comment.class);
     var xif = XMLInputFactory.newInstance();
-    xif.setProperty(XMLInputFactory.IS_SUPPORTING_EXTERNAL_ENTITIES, false);
+      
 
     if (webSession.isSecurityEnabled()) {
       xif.setProperty(XMLConstants.ACCESS_EXTERNAL_DTD, ""); // Compliant
       xif.setProperty(XMLConstants.ACCESS_EXTERNAL_SCHEMA, ""); // compliant
     }
-
+      
+    xif.setProperty(XMLInputFactory.IS_SUPPORTING_EXTERNAL_ENTITIES, false);
+    xif.setProperty(XMLInputFactory.SUPPORT_DTD, false);
     var xsr = xif.createXMLStreamReader(new StringReader(xml));
-
+      
     var unmarshaller = jc.createUnmarshaller();
     return (Comment) unmarshaller.unmarshal(xsr);
   }
